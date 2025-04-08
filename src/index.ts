25eb4d5bc701a44edf92f521c07060bc88f37a43
Matthew <everdein@gmail.com>
2025-04-08 15:34:37 -0400

    Model setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("hi there");
+// import axios, { AxiosResponse } from "axios";
+
+// axios.get("http://localhost:3000/users").then((response: AxiosResponse) => {
+//   console.log(response.data);
+// });
+
+import { Collection } from "./models/Collection";
+
+const collection = new Collection("http://localhost:3000/users");
+
+collection.on("change", () => {
+  console.log(collection);
+});
+
+collection.fetch();
